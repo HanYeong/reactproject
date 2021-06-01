ff29c7c84f7adcfe788037005a7663185f358df4
HanYeong <sav113@mcocoa.com>
2021-06-01 11:02:28 +0900

    배열 추가 제거 수정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,69 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
 import Counter from './Counter';
 import InputSample from './InputSample';
 import './App.css';
 import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 function App(){
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+  const { username, email } = inputs;
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+      active: true
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+      active: false
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com',
+      active: false
+    }
+  ]);
+  const nextId = useRef(4);
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    };
+    setInputs({
+      username: '',
+      email: ''
+    });
+    setUsers(users.concat(user));
+    nextId.current += 1;
+  };
+  const onRemove = id => {
+    setUsers(users.filter(user => user.id !== id));
+  };
+  const onToggle = id => {
+    setUsers(
+      users.map(user =>
+        user.id === id ? { ...user, active: !user.active } : user
+      )
+    );
+  };
   const name = 'react';
   const style = {
     backgroundColor: 'black',
@@ -28,7 +85,12 @@ function App(){
       </Wrapper>
       <Counter/>
       <InputSample/>
-      <UserList/>
+      <CreateUser username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+        />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
     </>
   );
 }
