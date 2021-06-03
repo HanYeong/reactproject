d95e73d99f4291ddf453b9621b8dbfe4ad316957
HanYeong <sav113@mcocoa.com>
2021-06-03 16:49:33 +0900

    todolist 구성

--- src/App.js
+++ src/App.js
@@ -1,139 +1,17 @@
-import React, { useCallback, useReducer, useRef, useMemo } from 'react';
-// import Hello from './Hello';
-// import Wrapper from './Wrapper';
-// import Counter from './Counter';
-// import InputSample from './InputSample';
-import './App.scss';
-import UserList from './UserList';
-import CreateUser from './CreateUser';
-import useInputs from './hooks/useInputs';
-import Button from './components/Button';
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
 
-function countActiveUsers(users){
-  console.log('활성 사용자 수를 세는중...');
-  return users.filter(user => user.active).length;
-}
-const initialState = {
-  users: [
-    {
-      id: 1,
-      username: 'velopert',
-      email: 'public.velopert@gmail.com',
-      active: true
-    },
-    {
-      id: 2,
-      username: 'tester',
-      email: 'tester@example.com',
-      active: false
-    },
-    {
-      id: 3,
-      username: 'liz',
-      email: 'liz@example.com',
-      active: false
-    }
-  ]
-};
-function reducer(state, action){
-  switch(action.type){
-    case 'CREATE_USER':
-      return {
-        users: state.users.concat(action.user)
-      };
-    case 'TOGGLE_USER':
-      return {
-        users: state.users.map(user =>
-          user.id === action.id ? {...user, active: !user.active} : user  
-        )
-      };
-    case 'REMOVE_USER':
-      return {
-        users: state.users.filter(user => user.id !== action.id)
-      };
-      default:
-        return state;
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
   }
-}
+`;
 
-export const UserDispatch = React.createContext(null);
-
-function App(){
-  const [{username, email}, onChange, reset] = useInputs({
-    username: '',
-    email: ''
-  });
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const nextId = useRef(4);
-  const {users} = state;
-
-  const onCreate = useCallback(() => {
-    dispatch({
-      type: 'CREATE_USER',
-      user: {
-        id: nextId.current,
-        username,
-        email
-      }
-    });
-    reset();
-    nextId.current += 1;
-  }, [username, email, reset]);
-
-  const onToggle = useCallback(id => {
-    dispatch({
-      type: 'TOGGLE_USER',
-      id
-    });
-  }, []);
-
-  const onRemove = useCallback(id => {
-    dispatch({
-      type: 'REMOVE_USER',
-      id
-    });
-  }, []);
-
-  const count = useMemo(() => countActiveUsers(users), [users]);
+function App() {
   return (
     <>
-      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
-      <UserList users={users} onToggle={onToggle} onRemove={onRemove} />
-      <div>활성사용자 수 : {count}</div>
-      <div className="buttons">
-        <Button size="large" onClick={() => console.log("clicked")}>BUTTON</Button>
-        <Button>BUTTON</Button>
-        <Button size="small">BUTTON</Button>
-      </div>
-      <div className="buttons">
-        <Button size="large" color="gray">
-          BUTTON
-        </Button>
-        <Button color="gray">BUTTON</Button>
-        <Button size="small" color="gray">
-          BUTTON
-        </Button>
-      </div>
-      <div className="buttons">
-        <Button size="large" color="pink">
-          BUTTON
-        </Button>
-        <Button color="pink">BUTTON</Button>
-        <Button size="small" color="pink">
-          BUTTON
-        </Button>
-      </div>
-      <div className="buttons">
-        <Button size="large" color="blue" outline>
-          BUTTON
-        </Button>
-        <Button color="gray" outline>
-          BUTTON
-        </Button>
-        <Button size="small" color="pink" outline>
-          BUTTON
-        </Button>
-      </div>
+      <GlobalStyle />
+      <div>안녕하세요</div>
     </>
   );
 }
